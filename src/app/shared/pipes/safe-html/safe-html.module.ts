b370f58fae0c6a0ed5b5cf436d15d799b94687a6
ouassimBenMosbah <wassimbenmosbah@gmail.com>
2021-05-21 03:17:07 +0200

    feat: create pipe for safe html

---
+++ src/app/shared/pipes/safe-html/safe-html.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SafeHtmlPipe } from './safe-html.pipe';
+
+@NgModule({
+  declarations: [SafeHtmlPipe],
+  imports: [CommonModule],
+  exports: [SafeHtmlPipe],
+})
+export class SafeHtmlModule {}
