1151ea92811309d29598547a86b5eb05756ca317
Ouassim Ben Mosbah <ouassim.benmosbah@perfect-memory.com>
2021-09-06 09:52:43 +0200

    refactor: improve variable names

--- src/app/features/stations-list/services/stations-list.service.ts
+++ src/app/features/stations-list/services/stations-list.service.ts
@@ -56,18 +56,18 @@ export class StationsListService {
   }
 
   private getDistanceFromLatLonInKm(lat1: number, lon1: number, lat2: number, lon2: number) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = this.deg2rad(lat2 - lat1); // deg2rad below
-    var dLon = this.deg2rad(lon2 - lon1);
-    var a =
+    const R = 6371; // Radius of the earth in km
+    const dLat: number = this.degToRadius(lat2 - lat1);
+    const dLon: number = this.degToRadius(lon2 - lon1);
+    const a: number =
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c; // Distance in km
-    return d;
+      Math.cos(this.degToRadius(lat1)) * Math.cos(this.degToRadius(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c: number = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distanceInKm: number = R * c;
+    return distanceInKm;
   }
 
-  private deg2rad(deg: number): number {
+  private degToRadius(deg: number): number {
     return deg * (Math.PI / 180);
   }
 }
