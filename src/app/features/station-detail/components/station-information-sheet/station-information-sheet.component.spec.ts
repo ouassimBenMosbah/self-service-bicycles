7b26c6c39e1025bb89f5338f1632df4adf6543af
Jeanella Pascual <jeanella.pascual@perfect-memory.com>
2021-09-08 12:15:31 +0200

    fix: Adds initial tests for station information sheet

--- src/app/features/station-detail/components/station-information-sheet/station-information-sheet.component.spec.ts
+++ src/app/features/station-detail/components/station-information-sheet/station-information-sheet.component.spec.ts
@@ -1,24 +1,59 @@
+import { SimpleChange } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Station } from 'src/app/core/interfaces/station.interface';
+import { IconComponent } from 'src/app/shared/components/icon/icon.component';
 
 import { StationInformationSheetComponent } from './station-information-sheet.component';
 
+export class MockTestService {}
 describe('StationInformationSheetComponent', () => {
   let component: StationInformationSheetComponent;
   let fixture: ComponentFixture<StationInformationSheetComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [StationInformationSheetComponent],
-    }).compileComponents();
-  });
+  const station: Station = {
+    station_id: 'test-station',
+    name: 'Test Station',
+    lat: 0,
+    lon: 0,
+    capacity: 12,
+    is_installed: true,
+    is_renting: true,
+    is_returning: false,
+    last_reported: 1,
+    num_bikes_available: 3,
+    num_bikes_disabled: 0,
+    num_docks_available: 12,
+    updatedAt: new Date(2021, 1, 1),
+  };
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        StationInformationSheetComponent,
+        IconComponent,
+      ],
+    });
     fixture = TestBed.createComponent(StationInformationSheetComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Properties', () => {
+    it('should display nothing when there is no station', () => {
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('.station-detail'))).toBeNull();
+    });
+
+    it('should display station name', () => {
+      component.station = station;
+      component.ngOnChanges({
+        station: new SimpleChange(undefined, station, true)
+      });
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('.station-detail__name')).nativeElement.textContent).toEqual('Test Station');
+    });
   });
 });
